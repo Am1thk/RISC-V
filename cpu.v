be45b4072c2d03a7179b74884a417acbc4bab02a
Amith <rathidesai12345@gmail.com>
2026-06-11 19:32:08 +0530

    Implement basic CPU module with program counter

---
+++ cpu.v
@@ -0,0 +1,15 @@
+module cpu(
+    input clk,
+    input reset,
+    output reg [31:0] pc
+);
+
+always @(posedge clk or posedge reset)
+begin
+    if(reset)
+        pc <= 0;
+    else
+        pc <= pc + 4;
+end
+
+endmodule
